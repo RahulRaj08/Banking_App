40f7a529ceba89207de1a6cd1d9edc1f76c67e9c
RahulRaj08 <rahulsusan@gmail.com>
2022-12-02 14:16:37 +0530

    initial push

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+
+  aim = "your perfect banking partner"
+
+  data = "Enter your account number: "
+
+  acno=''
+
+  psw=''
+ 
+  userDetails:any = {
+    1000:{acno:1000,username:"rahul",password:123, balance:0},
+    1001:{acno:1001,username:"amal",password:123,  balance:0},
+    1002:{acno:1002,username:"arun",password:123,  balance:0},
+    1003:{acno:1003,username:"gwen",password:123,  balance:0},
+    1004:{acno:1004,username:"MJ",password:123, balance:0},
+  }
+
+  // login(){
+
+  //   var acno = this.acno
+  //   var psw = this.psw
+  //   var userDetails = this.userDetails
+
+  //   if(acno in userDetails){
+  //     if(psw == userDetails[acno]["password"]){
+  //       alert('login success')
+  //     }
+  //     else{
+  //       alert('Incorrect password')
+  //     }
+  //   }
+  //   else{
+  //     alert('Incorrect Username')
+  //   }
+
+  // }
+
+  login(a:any, b:any){
+    this.acno = a.value
+    this.psw = b.value
+    
+
+    var acno = this.acno
+    var psw = this.psw
+    var userDetails = this.userDetails
+
+    if(acno in userDetails){
+      if(psw == userDetails[acno]["password"]){
+        alert('login success')
+      }
+      else{
+        alert('Incorrect password')
+      }
+    }
+    else{
+      alert('Incorrect Username')
+    }
+
+  }
+
+
+  acnoChange(event:any){
+    this.acno = event.target.value
+    console.log(this.acno);
+    
+
+  }
+
+  pswChange(event:any){
+    this.psw = event.target.value
+    console.log(this.psw);
+    
+  }
+
+}
